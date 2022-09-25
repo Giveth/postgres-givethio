7d28304ab6bbd3db786d0f626f0bddde77080a4e
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-09-25 11:39:49 +0330

    Add ci/cd config and dockerfile

---
+++ init-db/001-setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# create custom config
+# Remove last line "shared_preload_libraries='citus'"
+sed -i '$ d' /var/lib/postgresql/data/pgdata/postgresql.conf
+cat <<EOT >> /var/lib/postgresql/data/pgdata/postgresql.conf
+shared_preload_libraries='pg_cron'
+cron.database_name='givethio'
+EOT
+# Required to load pg_cron
+pg_ctl restart
+
