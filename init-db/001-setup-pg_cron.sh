d5eb402d53d7f069852fbd1295dd23cd7a589e28
Moe Shehab <52987806+mhmdksh@users.noreply.github.com>
2024-02-12 16:13:09 +0400

    Update 001-setup-pg_cron.sh
    Fixed missing colon in the config

--- init-db/001-setup-pg_cron.sh
+++ init-db/001-setup-pg_cron.sh
@@ -4,7 +4,7 @@
 # Remove last line "shared_preload_libraries='citus'"
 sed -i '$ d' /var/lib/postgresql/data/pgdata/postgresql.conf
 cat <<EOT >> /var/lib/postgresql/data/pgdata/postgresql.conf
-shared_preload_libraries = 'pg_cron,pg_trgm';
+shared_preload_libraries = 'pg_cron,pg_trgm'
 cron.database_name = 'givethio'
 EOT
 # Required to load pg_cron
